f8b2671ec3c675994febf8b5e4fcd20efad58fde
CharlesScrivens <cscrivens@horizon.csueastbay.edu>
2024-02-05 14:38:26 -0800

    Added more squares

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ export default function Square() {
   <button className="square">X</button>
   <button className="square">X</button>
   <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
   </> 
   );
 }
